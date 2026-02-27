eca020a61a4b097429da3a79ccbbd67ce1b57dfe
GusVia1 <Viacheslavgusarov4@gmail.com>
2026-02-27 21:39:41 +0300

    задание 2 и 3

---
+++ init/02_insert_data.sql.sql
@@ -0,0 +1,11 @@
+INSERT INTO cars (brand, model, year, color, price) VALUES
+('Toyota', 'Camry', 2020, 'Silver', 1500000.00),
+('BMW', 'X5', 2022, 'Black', 4500000.50),
+('Lada', 'Vesta', 2021, 'White', 800000.00),
+('Ford', 'Focus', 2019, 'Blue', 1200000.00),
+('Hyundai', 'Tucson', 2023, 'Gray', 2800000.00),
+('Kia', 'Sportage', 2022, 'Red', 2600000.00),
+('Mercedes', 'E-Class', 2021, 'Black', 5500000.00),
+('Audi', 'Q5', 2024, 'White', 5800000.00),
+('Volkswagen', 'Passat', 2020, 'Silver', 2100000.00),
+('Nissan', 'Qashqai', 2022, 'Blue', 2400000.00);
